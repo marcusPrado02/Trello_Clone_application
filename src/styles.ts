7775568d7b5f016e6c4fc19631975b4d9dd6a837
marcusPrado02 <silvamarcusprado@gmail.com>
2022-07-10 15:32:52 -0300

    Components style

---
+++ src/styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components'
+
+//  ________________________________________
+//        Styles for App
+//  ________________________________________
+
+export const AppContainer = styled.div`
+    align-items: flex-start;
+    background-color: #3179ba;
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+    padding: 20px;
+    width: 100%;
+`
+//  ________________________________________
+
+
+
+
+
+//  ________________________________________
+//        Styles for Columns
+//  ________________________________________
+
+export const ColumnContainer = styled.div`
+    background-color: #ebecf0;
+    width: 300px;
+    min-height: 40px;
+    margin-right: 20px;
+    border-radius: 3px;
+    padding: 8px 8px;
+    flex-grow: 0;
+`
+
+export const ColumnTitle = styled.div`
+    padding: 6px 16px 12px;
+    font-weight: bold;
+`
+//  ________________________________________
+
+
+
+
+
+//  ________________________________________
+//        Styles for Cards
+//  ________________________________________
+
+export const CardContainer = styled.div`
+    background-color: #fff;
+    cursor: pointer;
+    margin-bottom: 0.5rem;
+    padding: 0.5rem 1rem;
+    ,ax-width: 300px;
+    border-radius: 3px;
+    box-shadow: #091e4240 0px 1px 0px 0px;
+`
+
+//  ________________________________________
+
+
+
+
+
+
+
+
+
+
