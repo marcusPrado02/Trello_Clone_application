772a7f8ac3520b253389111b0970db869495b766
marcusPrado02 <silvamarcusprado@gmail.com>
2022-07-11 09:03:58 -0300

    Create useFocus

---
+++ src/utils/useFocus.ts
@@ -0,0 +1,12 @@
+import { useRef, useEffect } from "react"
+
+
+export const useFocus = () => {
+    const ref = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        ref.current?.focus()
+    })
+
+    return ref
+}
