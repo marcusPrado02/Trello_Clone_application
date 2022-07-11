9ff2bf7870131c1cc437184a3130828d10d60073
marcusPrado02 <silvamarcusprado@gmail.com>
2022-07-11 09:08:56 -0300

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App } from "./App";
+import { AppStateProvider} from "./AppStateContext";
+import { DndProvider } from "react-dnd"
+import Backend from "react-dnd-html5-backend"
+
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root'),
+    <DndProvider backend={Backend} >
+        <AppStateProvider>
+            <App />
+        </AppStateProvider>
+    </DndProvider>,
+    document.getElementById("root"),
 )
 
-//serviceWorker.unregister();
 
 
 
