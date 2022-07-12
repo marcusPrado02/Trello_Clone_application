293925fb1c606c059e04f5b6e3558ee06bbe5367
marcusPrado02 <silvamarcusprado@gmail.com>
2022-07-12 09:37:41 -0300

    Update Column component

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,22 +1,44 @@
-import React from "react"
+import React,{ useRef } from "react"
 import  { ColumnContainer, ColumnTitle } from "../../styles"
 import { AddNewItem } from "../AddNewItem/AddNewItem"
 import { useAppState } from "../../AppStateContext"
 import { Card } from "../Card/Card"
+import { useItemDrag } from "../../useItemDrag"
+import { useDrop } from "react-dnd"
+
 
 // Column Props
 interface ColumnProps {
     text: string
     index: number
+    id: string
 }
 
 
 // Column component
-export const Column = ({ text, index,id }: ColumnProps) => {    
+export const Column = ({ text, index,id }: ColumnProps) => {
+    const [, drop] = useDrop({
+        accept: "COLUMN",
+            hover(item: DragItem) {
+                const dragIndex = item.index
+                const hoverIndex = index
+                if (dragIndex === hoverIndex) {
+                    return
+                }
+                dispatch({ type: "MOVE_LIST", payload: { dragIndex, hoverIndex } \
+            })
+            item.index = hoverIndex
+        }
+    })
+
     const { state, dispatch } = useAppState()
+    const ref = useRef<HTMLDivElement>(null)
+    const drag = useItemDrag(ref)
+
+    drag(drop(ref))
 
     return (
-        <ColumnContainer>
+        <ColumnContainer ref={ref} isHidden={isHidden(state.draggedItem, "COLUMN", id)}>
             <ColumnTitle>{text}</ColumnTitle>
             {state.lists[index].tasks.map(task => (
                 <Card text={task.text} key={task.id} />
