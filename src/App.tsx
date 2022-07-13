46508b553745b9b6b31af9afc5dd9cb0ea9e699f
marcusPrado02 <silvamarcusprado@gmail.com>
2022-07-13 11:39:01 -0300

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,33 @@ import { Column } from "./components/Column/Column";
 import { Card } from "./components/Card/Card";
 import { AddNewItem } from "./components/AddNewItem/AddNewItem"
 import { useAppState } from "./AppStateContext"
+import { CustomDragLayer } from "./CustomDragLayer"
 
-export const App = () => {
+
+interface AppContainerProps {
+    children: React.ReactNode | React.ReactNode[];
+}
+
+
+const App = (props: AppContainerProps) => {
     const { state, dispatch } = useAppState()
     
     return (
-        <AppContainer>
-            {state.lists.map((list, i) => {
-                <Column text={list.text} key={list.id} index={i} />
-            })}
-            <AddNewItem 
-                toggleButtonText=" + Add another list" 
-                onAdd={text => dispatch({ type: "ADD_LIST", payload: text })} 
-            />
-        </AppContainer>
+        <App>
+            <CustomDragLayer />
+                {state.lists.map((list, i) => {
+                    <Column text={list.text} key={list.id} index={i} id={list.id}/>
+                })}
+                <AddNewItem 
+                    toggleButtonText=" + Add another list" 
+                    onAdd={text => dispatch({ type: "ADD_LIST", payload: text })} 
+                />
+        </App>
     )
 }
 
 
+export default App
 
 
 
